1deaec87d22e0208c05c0cce5b60fd9f27d42143
DennisRono <dennisrkibet@gmail.com>
2024-07-30 12:15:36 +0300

    worked on props lab

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Links from './Links'
+
+function About({ links, bio }) {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      {bio ? <p>{bio}</p> : null}
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links github={links.github} linkedin={links.linkedin} />
+    </div>
+  )
+}
+
+export default About
